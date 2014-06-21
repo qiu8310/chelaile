3e2237e301e7922d97ae74847c488acbd2689833
zhonglei qiu <mora@liulishuo.com>
2014-06-21 11:14:57 +0800

    添加 player

---
+++ app/scripts/libs/player.js
@@ -0,0 +1,51 @@
+define(function() {
+
+  function Player(src, cfg) {
+    var player, name = 'audio-player';
+    player = document.createElement('Audio');
+    player.className = name;
+    player.style.display = 'none';
+
+    if (cfg)
+      for (var key in cfg)
+        if (cfg.hasOwnProperty(key)) player[key] = cfg[key];
+
+    document.body.appendChild(player);
+    this.player = player;
+
+    this.player.src = src;
+  }
+
+  Player.prototype = {
+    volume: function(val) {
+      return val ? (this.player.volume = val) : this.player.volume;
+    },
+    play: function() {
+      this.player.play();
+    },
+    pause: function() {
+      this.player.pause();
+    },
+    time: function(val) {
+      return val ? (this.player.currentTime = val) : this.player.currentTime;
+    },
+    totalTime: function() {
+      return this.player.duration;
+    },
+    on: function(type, func) {
+      var i,
+        self = this,
+        types = type.split(/\s+/),
+        handler = function(e) {
+          // chrome bug， 音频结束的时候同时会触发 paused 和 ended 事件
+          if (self.player.ended && e.type === 'pause') return;
+          func.apply(self, Array.prototype.slice.call(arguments));
+        };
+
+      for (i = 0; i < types.length; ++i)
+        if (types[i]) this.player.addEventListener(types[i], handler, false);
+    }
+  };
+
+  return Player;
+});
