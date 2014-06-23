ab8aa7d22d17e2f85ecaead193acc5b18b1811eb
zhonglei qiu <mora@liulishuo.com>
2014-06-23 14:20:52 +0800

    update player.js

--- app/scripts/libs/player.js
+++ app/scripts/libs/player.js
@@ -1,61 +1,68 @@
-define(function() {
-
+define(function () {
+  'use strict';
   function Player(src, cfg) {
-    var player, isplay = false, name = 'audio-player';
+    var player, isplay = false, name = 'audio-player', key;
     player = document.createElement('Audio');
     player.className = name;
     player.style.display = 'none';
 
-    if (cfg)
-      for (var key in cfg)
-        if (cfg.hasOwnProperty(key)) player[key] = cfg[key];
+    if (cfg) {
+      for (key in cfg) {
+        if (cfg.hasOwnProperty(key)) {
+          player[key] = cfg[key];
+        }
+      }
+    }
 
     document.body.appendChild(player);
     this.player = player;
 
     this.player.src = src;
+    this.isplay = isplay;
   }
 
   Player.prototype = {
-    volume: function(val) {
+    volume: function (val) {
       return val ? (this.player.volume = val) : this.player.volume;
     },
-    isPlaying: function() {
-      return isplay;
+    isPlaying: function () {
+      return this.isplay;
     },
-    play: function() {
-      isplay = true;
+    play: function () {
+      this.isplay = true;
       this.player.play();
     },
-    pause: function() {
-      isplay = false;
+    pause: function () {
+      this.isplay = false;
       this.player.pause();
     },
-    toggle: function() {
-      if (isplay) {
+    toggle: function () {
+      if (this.isplay) {
         this.pause();
       } else {
         this.play();
       }
     },
-    time: function(val) {
+    time: function (val) {
       return val ? (this.player.currentTime = val) : this.player.currentTime;
     },
-    totalTime: function() {
+    totalTime: function () {
       return this.player.duration;
     },
-    on: function(type, func) {
-      var i,
-        self = this,
-        types = type.split(/\s+/),
-        handler = function(e) {
-          // chrome bug， 音频结束的时候同时会触发 paused 和 ended 事件
-          if (self.player.ended && e.type === 'pause') return;
-          func.apply(self, Array.prototype.slice.call(arguments));
-        };
+    on: function (type, func) {
+      var i, self = this, types = type.split(/\s+/), handler = function (e) {
+        // chrome bug， 音频结束的时候同时会触发 paused 和 ended 事件
+        if (self.player.ended && e.type === 'pause') {
+          return;
+        }
+        func.apply(self, Array.prototype.slice.call(arguments));
+      };
 
-      for (i = 0; i < types.length; ++i)
-        if (types[i]) this.player.addEventListener(types[i], handler, false);
+      for (i = 0; i < types.length; ++i) {
+        if (types[i]) {
+          this.player.addEventListener(types[i], handler, false);
+        }
+      }
     }
   };
 
