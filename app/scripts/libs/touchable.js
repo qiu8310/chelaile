7b793f2797bb39e34c9248393ae2c0e0ff231ab2
zhonglei qiu <mora@liulishuo.com>
2014-06-26 15:26:42 +0800

    add touchable component

---
+++ app/scripts/libs/touchable.js
@@ -0,0 +1,21 @@
+define('libs/utils', function(utils) {
+
+  function touchstart (e) {
+    touchend();
+    if (e.target.classList.contains('touchable')) {
+      e.target.classList.add('touched');
+    }
+  }
+
+
+  function touchend () {
+    utils.__('.touched').forEach(function(ele) {
+      ele.classList.remove('touched');
+    });
+  }
+
+  document.addEventListener('touchstart', touchstart, false);
+  document.addEventListener('touchend', touchend, false);
+  document.addEventListener('touchcancel', touchend, false);
+
+});
